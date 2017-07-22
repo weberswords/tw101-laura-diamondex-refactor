d5bcfd799b90bdf4fa439852681884e19e3c0793
Webs <weberswords@gmail.com>
2017-07-22 13:46:00 -0700

    Removed drawADiamondWithYourNameCustom call; changed numSpaces in drawAnIsoscelesTriangleReverse to initialize to 1; replaced for loops in drawADiamond and drawADiamondWithYourName with calls to methods drawAnIsoscelesTriangle and drawAnIsoscelesTriangleReverse.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -8,9 +8,7 @@ public class DiamondExercises {
         System.out.println();
         drawADiamondWithYourName(3);
         System.out.println();
-        drawADiamondWithYourNameCustom(8, "TWU");
     }
-
 
     private static void drawAnIsoscelesTriangle(int n) {
 
@@ -31,7 +29,7 @@ public class DiamondExercises {
     }
 
     private static void drawAnIsoscelesTriangleReverse(int n) {
-        int numSpaces = 0;
+        int numSpaces = 1;
         int numStars = (n*2)-1;
 
         for (int i=0; i<n; i++) {
@@ -49,58 +47,14 @@ public class DiamondExercises {
 
 
     private static void drawADiamond(int n) {
-
         drawAnIsoscelesTriangle(n);
-        int numSpaces = 0;
-        int numStars = ((n-1)*2)-1;
-
-        for (int i=0; i<n-1; i++) {
-            System.out.print(" ");
-            for (int j=0; j<numSpaces; j++) {
-                System.out.print(" ");
-            }
-            for (int k=0; k<numStars; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            numSpaces++;
-            numStars-=2;
-        }
+        drawAnIsoscelesTriangleReverse(n-1);
 
     }
 
     private static void drawADiamondWithYourName(int n) {
-        int numSpaces = ((n-1)*2-1)/2;
-        int numStars = 1;
-
-        for (int i=0; i<n-1; i++) {
-            System.out.print(" ");
-            for (int j = 0; j < numSpaces; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < numStars; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            numSpaces--;
-            numStars += 2;
-        }
+        drawAnIsoscelesTriangle(n);
         System.out.println("Laura");
-        numSpaces = 0;
-        numStars = ((n-1)*2)-1;
-
-        for (int i=0; i<n-1; i++) {
-            System.out.print(" ");
-            for (int j = 0; j < numSpaces; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < numStars; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            numSpaces++;
-            numStars -= 2;
+        drawAnIsoscelesTriangleReverse(n-1);
         }
-    }
-    
 }
