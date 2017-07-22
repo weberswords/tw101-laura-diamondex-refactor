a23491bc0fccf1da24c98e20fc412e0610da7ade
Webs <weberswords@gmail.com>
2017-07-22 13:23:29 -0700

    Removed extra custom name diamond method (dispensable speculative generality)

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -3,7 +3,6 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
-
         System.out.println();
         drawADiamond(8);
         System.out.println();
@@ -103,45 +102,5 @@ public class DiamondExercises {
             numStars -= 2;
         }
     }
-
-
-    private static void drawADiamondWithYourNameCustom(int n, String inputName) {
-        int numSpaces = ((n-1)*2-1)/2;
-        int numStars = 1;
-
-        for (int i=0; i<n-1; i++) {
-            System.out.print(" ");
-            for (int j = 0; j < numSpaces; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < numStars; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            numSpaces--;
-            numStars += 2;
-        }
-        int nameLength = inputName.length();
-        int nameSpaces = ((n*2-1)-nameLength)/2;
-        for (int l=0; l<nameSpaces; l++) {
-            System.out.print(" ");
-        }
-        System.out.println(inputName);
-
-        numSpaces = 0;
-        numStars = ((n-1)*2)-1;
-
-        for (int i=0; i<n-1; i++) {
-            System.out.print(" ");
-            for (int j = 0; j < numSpaces; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < numStars; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            numSpaces++;
-            numStars -= 2;
-        }
-    }
+    
 }
